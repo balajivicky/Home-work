04342528ed7cb25cdc8b555728ffa8d4fc951d21
Balajivicky <mbalajivicky@gmail.com>
2026-03-10 08:26:49 +0530

    Packages

---
+++ 09-03-26 Packages/PrimeNumberProgram.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class PrimeNumberProgram 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if(n <= 1) 
+		{
+            isPrime = false;
+        }
+
+        for(int i = 2; i <= n/2; i++) 
+		{
+            if(n % i == 0) 
+			{
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime)
+            System.out.println(n + " is a Prime Number");
+        else
+            System.out.println(n + " is NOT a Prime Number");
+    }
+}
